2512d9ed51ceeac23d80569d2f7d7ebf8b8856a5
tunikakeks <keksdev9513@gmail.com>
2026-02-11 08:25:09 +0100

    add /RedTrack for gh pages

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+const isPages = process.env.BUILD_TARGET === "pages";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -7,6 +9,9 @@ const nextConfig: NextConfig = {
     unoptimized: true
   },
   output: 'export',
+  basePath: isPages ? "/RedTrack" : "",
+  assetPrefix: isPages ? "/RedTrack/" : "",
+  trailingSlash: true,
 };
 
 export default nextConfig;
